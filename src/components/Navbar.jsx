f86b028d7c6ca9d7f3adf16bb0f74dd76174dafa
R1K4S <ricardohsr1@gmail.com>
2026-03-24 12:29:55 -0300

    ->New Game

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="w-full flex-row">
+      <p>Portifolio.exe/Rikas</p>
+      <div>
+        <p>Sobre</p>
+        <p>Skills</p>
+      </div>
+    </div>
+  );
+}
