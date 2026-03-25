c76f847db02614ff84c62de9e40ea5c92ada59f4
R1K4S <ricardohsr1@gmail.com>
2026-03-25 18:51:40 -0300

    First Save:Hero and Navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="w-full h-fit flex justify-center items-center font-display bg-tertiary">
+      <h1 className="text-[11rem]">Rikas</h1>
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 export default function Navbar() {
   return (
-    <div className="w-full flex-row">
-      <p>Portifolio.exe/Rikas</p>
-      <div>
-        <p>Sobre</p>
-        <p>Skills</p>
+    <div className="w-full h-fit flex items-center justify-between p-2">
+      <div className="flex">
+        <p className="text-[10px] font-body">Portifolio.exe/</p>
+        <p className="text-[10px] font-display">Rikas</p>
       </div>
+      <ul className="flex justify-evenly gap-2 p-2 text-xs font-body border">
+        <li>Sobre</li>
+        <p>|</p>
+        <li>Skills</li>
+        <p>|</p>
+        <li>Projetos</li>
+        <p>|</p>
+        <li>Contato</li>
+      </ul>
     </div>
   );
 }
